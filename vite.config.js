9abab19e6442a7759d5f94ad0cbce8238740987a
Antoine POPINEAU <antoine@popineau.eu>
2023-07-07 16:48:53 +0200

    Set up build, release and artifacts.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib.ts"),
+      name: "@apognu/diagascode",
+      fileName: (format) => `diagascode.${format}.js`,
+    },
     src: "./src",
     outDir: "./dist",
   },
